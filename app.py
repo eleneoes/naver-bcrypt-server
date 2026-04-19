7f4eabf88fe0029c502e9563444059dec45e539b
eleneoes <mytem73@gmail.com>
2026-04-19 16:57:09 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import bcrypt
+import time
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def generate_sign():
+    data = request.get_json()
+    client_id = data['clientId']
+    client_secret = data['clientSecret'].encode('utf-8')
+    timestamp = str(int(time.time() * 1000))
+    password = (client_id + '_' + timestamp).encode('utf-8')
+    hashed = bcrypt.hashpw(password, client_secret)
+    sign = hashed.decode('utf-8')
+    return jsonify({'timestamp': timestamp, 'client_secret_sign': sign})
+
+if __name__ == '__main__':
+    app.run()
